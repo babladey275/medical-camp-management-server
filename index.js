d42e7288539209ff634ad236909f1480374af06c
babladey275 <babladey275@gmail.com>
2025-01-25 19:08:14 +0530

    added GET route to fetch camp details by ID

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -81,6 +81,13 @@ async function run() {
 
       const campsResult = await result.toArray();
       res.send(campsResult);
+    });
+
+    app.get("/camps/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await campCollection.findOne(query);
+      res.send(result);
     });
   } finally {
     // Ensures that the client will close when you finish/error
