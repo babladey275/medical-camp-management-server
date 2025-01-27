be00e25d6276121e3b7011700f4c3b6b2b43b8d6
babladey275 <babladey275@gmail.com>
2025-01-27 16:07:44 +0530

    check for existing user before insertion

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
     // user related api
     app.post("/users", async (req, res) => {
       const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
